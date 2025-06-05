ec7edafdf945cc71ad55d0e25edde2e506b71edc
Saksham Kapoor <sakshamkapoor810@gmail.com>
2025-06-05 16:21:01 +0530

    Clerk authentication complete in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { Button } from "@/components/ui/button";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { PublicLayout } from "@/layout/public-layout";
+import HomePage from "@/routes/home";
+import AuthenticationLayout from "@/layout/auth-layout";
+import { SignInPage } from "./routes/sing-in";
+import { SignUpPage } from "./routes/sing-up";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/layout/protected-routes.tsx
@@ -0,0 +1,18 @@
+import { LoaderPage } from "@/routes/loader-page";
+import { useAuth } from "@clerk/clerk-react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
+  const { isLoading, isSignedIn } = useAuth();
+
+  if (isLoading) {
+    return <LoaderPage />;
+  }
+
+  if (isSignedIn) {
+    return <Navigate to={"/singin/*"} replace />;
+  }
+  return children;
+};
+
+export default ProtectedRoutes;
